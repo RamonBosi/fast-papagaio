966e7da9fe334f996c9da635e42ec13bcaed2d4d
Ramon Bosi <ramombosi2015@gmail.com>
2023-02-06 16:26:54 -0300

    Criando o estilo global

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+  :root{
+    --elements-background-color: rgba(83, 62, 133);
+    --page-background-color: rgba(243, 233, 221);
+    --default-white: rgba(255, 255, 255);
+    --default-black: rgba(0, 0, 0);
+  }
+`
