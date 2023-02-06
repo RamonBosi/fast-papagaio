542954a8696b71776d006c1a95e13b3178605da2
Ramon Bosi <ramombosi2015@gmail.com>
2023-02-06 15:24:46 -0300

    configurando e testando o bootstrap e react-bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+
 export default function Home() {
   return (
-   <h1>Ola mundo</h1>
+   <>
+    <div className="d-flex mb-3">
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+    </div>
+
+   <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    
+   </>
   )
 }
