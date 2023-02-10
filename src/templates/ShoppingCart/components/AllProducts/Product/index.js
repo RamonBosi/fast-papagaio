d64c26bba5922ac9e0f5b299f7fc0aa2032956eb
Ramon Bosi <ramombosi2015@gmail.com>
2023-02-10 17:49:37 -0300

    Estilizando a página 'carrinho de compra'

--- src/templates/ShoppingCart/components/AllProducts/Product/index.js
+++ src/templates/ShoppingCart/components/AllProducts/Product/index.js
@@ -1,22 +1,22 @@
-export function Product({name,value,amount}) {
+import { ScTruncatedParagraph } from "./styles";
+
+export function Product({ name, value, amount }) {
 
   return (
     <div>
-      <div className="d-flex justify-content-between">
-        <div className="d-flex">
+      <div className="d-flex flex-column flex-sm-row justify-content-sm-between">
+        <div className="d-flex gap-2">
           <div>
-            <div>
-              <div style={{width: '48px', height:'48px', backgroundColor: 'darkslategray'}}></div>
-              </div>
-            <div style={{minWidth:0}} >
-              <p className="text-truncate">{name}</p>
-            </div>
+            <div style={{ width: '48px', height: '48px', backgroundColor: 'darkslategray' }}></div>
           </div>
+          <ScTruncatedParagraph className="text-truncate">
+            {name}
+          </ScTruncatedParagraph>
         </div>
-        <div>
-          <p>Valor: R$ {value}</p>
+        <div className="d-flex justify-content-around gap-sm-2">
           <div>Seletor de quantidade</div>
-          <div>Estoque: {amount}</div>
+          <p>R$ {value}</p>
+          <p className="d-none">Estoque: {amount}</p>
         </div>
       </div>
       <div>
---
+++ src/templates/ShoppingCart/components/AllProducts/Product/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ScTruncatedParagraph = styled.p`
+  width: 100%;
+
+  @media(min-width: 576px){
+    width: 200px;
+  }
+`
