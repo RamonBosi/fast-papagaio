7b8c92d39d14a99eb4a0f487a79e46c0003e2d04
Ramon Bosi <ramombosi2015@gmail.com>
2023-02-14 19:21:09 -0300

    Estilizando os campos de: Frete, cupom e valor total da página 'carrinho de compras'

--- src/pages/carrinho-de-compra/components/AllProducts/index.js
+++ src/pages/carrinho-de-compra/components/AllProducts/index.js
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react"
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
 import jsonProducts from '@/mocks/products.json'
 import { Product } from "./Product"
+import { ScButton } from "@/styles/styledComponents/components/Button"
+import { primaryButton } from "@/styles/styledComponents/components/Button/theme/primaryButton"
+import { ScCupomInput } from "./styles"
+import { secundaryButton } from "@/styles/styledComponents/components/Button/theme/secundaryButton"
 
 export function AllProducts() {
 
@@ -24,6 +31,34 @@ export function AllProducts() {
   return (
     <div className="d-flex flex-column gap-2 p-2">
       {loadProducts || <></>}
+      <div className="d-flex flex-column align-items-end gap-2 fs-5">
+        <div className="d-flex gap-2">
+          <p>Frete:</p>
+          <strong>R$20,00</strong>
+        </div>
+        <div>
+          <div className="d-flex gap-2 justify-content-end align-items-center">
+            <label className="fw-bold">Cupom</label>
+            <ScCupomInput type='text' />
+            <ScButton theme={primaryButton}>Aplicar</ScButton>
+          </div>
+          <OverlayTrigger
+            placement="bottom"
+            delay={{ show: 250, hide: 400 }}
+            overlay={(props) => (
+              <Tooltip id="button-tooltip" {...props}>
+                Nome do desenvolvedor desse site.
+              </Tooltip>
+            )}
+          >
+            <ScButton theme={secundaryButton}>Dica</ScButton>
+          </OverlayTrigger>
+        </div>
+        <div className="d-flex gap-2">
+          <p>Total:</p>
+          <strong>R$1000</strong>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/pages/carrinho-de-compra/components/AllProducts/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const ScCupomInput = styled.input`
+  width: 40%;
+  height: 40px;
+  padding: 4px;
+  border: 1px solid rgba(83, 62, 133);
+
+  &:focus-visible{
+    outline: 1.5px solid rgba(83, 62, 133);
+  }
+`
+
