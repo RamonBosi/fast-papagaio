4106e87d746bb14430f8a285d2dbb8ea90bb9c69
Ramon Bosi <ramombosi2015@gmail.com>
2023-02-14 15:52:55 -0300

    Criando media queries para o texto truncado

--- src/pages/carrinho-de-compra/components/AllProducts/Product/styles.js
+++ src/pages/carrinho-de-compra/components/AllProducts/Product/styles.js
@@ -6,4 +6,24 @@ export const ScTruncatedParagraph = styled.p`
   @media(min-width: 576px){
     width: 30ch;
   }
+
+  @media(min-width: 700px){
+    width: 40ch;
+  }
+
+  @media(min-width: 850px){
+    width: 50ch;
+  }
+
+  @media(min-width: 920px){
+    width: 60ch;
+  }
+
+  @media(min-width: 1050px){
+    width: 70ch;
+  }
+
+  @media(min-width: 1300px){
+    width: 85ch;
+  }
 `
