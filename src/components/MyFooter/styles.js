94c4222fa9fae53aa611474513823fd415825360
Ramon Bosi <ramombosi2015@gmail.com>
2023-03-14 08:55:59 -0300

    Efeito de cursor:pointer nos icones das redes sociais de Footer e aplicação de margin no container da tagMain

--- src/components/MyFooter/styles.js
+++ src/components/MyFooter/styles.js
@@ -2,4 +2,8 @@ import styled from 'styled-components'
 
 export const ScFooter = styled.footer`
   background-color: var(--elements-background-color);
+
+  i{
+    cursor: pointer;
+  }
 `
--- src/components/TagMain/index.js
+++ src/components/TagMain/index.js
@@ -4,7 +4,7 @@ export function TagMain({ children }) {
 
   return (
     <main>
-      <Container fluid='xl' className="p-2 d-flex flex-column">
+      <Container fluid='xl' className="p-2 d-flex flex-column my-4">
         {children}
       </Container>
     </main>
