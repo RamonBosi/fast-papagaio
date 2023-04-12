5366ba562702a2abc1d9abae9e3b224501179fa1
Ramon Bosi <ramombosi2015@gmail.com>
2023-04-12 18:01:43 -0300

    Refatorando o componente RatingStars

--- src/components/RatingStars/index.js
+++ src/components/RatingStars/index.js
@@ -1,29 +1,22 @@
-import { useEffect, useState } from "react"
 import { ScRatingStars } from "./styles"
 
 export const RatingStars = ({ stars, theme, className = null }) => {
 
-  const [showStars, setShowStars] = useState(null)
+  const maxStars = 5
 
-  useEffect(() => {
-    const maxStars = 5
+  let loadStars = []
 
-    let loadStars = []
-
-    for (let n = 1; n <= maxStars; n++) {
-      if (n <= stars) {
-        loadStars.push(<i key={n} className="bi bi-star-fill"></i>)
-      } else {
-        loadStars.push(<i key={n} className="bi bi-star"></i>)
-      }
+  for (let n = 1; n <= maxStars; n++) {
+    if (n <= stars) {
+      loadStars.push(<i key={n} className="bi bi-star-fill"></i>)
+    } else {
+      loadStars.push(<i key={n} className="bi bi-star"></i>)
     }
-
-    setShowStars(loadStars)
-  }, [])
+  }
 
   return (
     <ScRatingStars className={className} theme={theme}>
-      {showStars}
+      {loadStars}
     </ScRatingStars>
   )
 }
