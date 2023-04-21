16a811f9a73ae74125c15795a2b5cf7844df3e2a
Ramon Bosi <ramombosi2015@gmail.com>
2023-04-21 17:26:43 -0300

    Mudando o titulo da página e tentando trocar o favicon

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -1,9 +1,13 @@
 import { GlobalStyles } from "@/styles/GlobalStyles"
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyles/>
+      <Head>
+        <title>FastPapagaio</title>
+      </Head>
+      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
